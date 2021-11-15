1df9a9a96b200e7ae3c25ec11a3c2deac5f9be6e
Team E-Commerce <team-e-commerce@e-spirit.com>
2021-11-15 11:10:29 +0000

    "Updating to v4.0.0"

--- src/lib/config/fs-spartacus-bridge-config.ts
+++ src/lib/config/fs-spartacus-bridge-config.ts
@@ -1,8 +1,20 @@
 import { CaaS } from './caas';
 import { FirstSpiritManagedPage } from './first-spirit-managed-page';
+import { Injectable } from "@angular/core";
+import { Config } from "@spartacus/core";
 
-export class FsSpartacusBridgeConfig {
-  caas: CaaS;
-  firstSpiritManagedPages: FirstSpiritManagedPage[];
+@Injectable({
+  providedIn: 'root',
+  useExisting: Config,
+})
+export abstract class FsSpartacusBridgeConfig {
+  caas?: CaaS;
+  firstSpiritManagedPages?: FirstSpiritManagedPage[];
   fallbackLanguage?: string;
+}
+
+// Typescript Interface Augmentation
+declare module '@spartacus/core' {
+  // tslint:disable-next-line
+  interface Config extends FsSpartacusBridgeConfig {}
 }
